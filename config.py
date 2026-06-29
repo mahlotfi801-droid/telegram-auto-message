da049e25afd6415a1db0e435f921028dd84d0f24
mahlotfi801-droid <mah.lotfi801@gmail.com>
2026-06-29 13:07:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ CHAT_ID = "1636617652"
 
 # التوقيتات (بنظام 24 ساعة)
 MORNING_HOUR = 5   # الساعة 7 صباحاً
-AFTERNOON_HOUR = 11 # الساعة 5 عصراً
+AFTERNOON_HOUR = 12 # الساعة 5 عصراً
 NIGHT_HOUR = 21    # الساعة 11 ليلاً
