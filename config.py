2977859743420fd078dd8843ae4185d241681ce6
mahlotfi801-droid <mah.lotfi801@gmail.com>
2026-06-30 01:03:44 +0300

    config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ BOT_TOKEN = "8929911107:AAHWMRbeYLZSyiIeQhmEAdDZvz3Ai53Nc40"
 CHAT_ID = "1636617652"
 
 # التوقيتات (بنظام 24 ساعة)
-MORNING_HOUR = 5   # الساعة 7 صباحاً
-AFTERNOON_HOUR = 12 # الساعة 5 عصراً
-NIGHT_HOUR = 21    # الساعة 11 ليلاً
+MORNING_HOUR = 4   # الساعة 7 صباحاً
+AFTERNOON_HOUR = 14 # الساعة 5 عصراً
+NIGHT_HOUR = 20    # الساعة 11 ليلاً
