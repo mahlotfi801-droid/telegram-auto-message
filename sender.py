6e0b2fb0b8cfaeb845062cd08502e53c52784a6a
mahlotfi801-droid <mah.lotfi801@gmail.com>
2026-06-30 12:26:47 +0300

    sender.py

--- sender.py
+++ sender.py
@@ -3,13 +3,17 @@ import time
 from config import BOT_TOKEN, CHAT_ID
 
 def send_telegram_message(text):
+    if not BOT_TOKEN or not CHAT_ID:
+        print("❌ التوكن أو الـ CHAT_ID مش موجودين!")
+        return False
+        
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": text}
-    
-    # محاولات إعادة الإرسال لو حصل مشكلة في النت
+
     for wait in [0, 30, 60]:
         try:
-            if wait > 0: time.sleep(wait)
+            if wait > 0:
+                time.sleep(wait)
             response = requests.post(url, data=payload)
             if response.status_code == 200:
                 return True
