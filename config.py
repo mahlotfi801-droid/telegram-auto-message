c2a7acfa355ded9acc8b958eddbe1a993f98761d
mahlotfi801-droid <mah.lotfi801@gmail.com>
2026-06-30 19:31:33 +0300

    config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ CHAT_ID = os.getenv('CHAT_ID')
 # المواعيد بتوقيت مصر (24 ساعة)
 MORNING_HOUR = 18    # 11 صباحاً
 AFTERNOON_HOUR = 19  # 12 ظهراً
-NIGHT_HOUR = 23      # 1 مساءً
+NIGHT_HOUR = 20      # 1 مساءً
 
 # فترة السماح (بالدقائق)
 GRACE_MINUTES = 40   # 50 دقيقة تسامح
