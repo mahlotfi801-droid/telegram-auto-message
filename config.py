3ceb074fa9e1064d6ea1b3e0949ccd0ec6721352
mahlotfi801-droid <mah.lotfi801@gmail.com>
2026-07-01 16:29:12 +0300

    config.py

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 import os
 
-# التوكن والـ ID جايين من GitHub Secrets
+# التوكن والـ ID من GitHub Secrets
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
+CHAT_ID = os.getenv('CHAT_ID')  # أو CHAT_IDS
 
-# المواعيد بتوقيت مصر (24 ساعة)
-MORNING_HOUR = 18    # 11 صباحاً
-AFTERNOON_HOUR = 20  # 12 ظهراً
-NIGHT_HOUR = 21      # 1 مساءً
+# المواعيد بتوقيت مصر (8 مواعيد)
+MORNING_HOUR = 12    # 12 ظهراً
+AFTERNOON_HOUR = 13  # 1 ظهراً
+NIGHT_HOUR = 14      # 2 ظهراً
+EVENING_HOUR_1 = 15  # 3 عصراً
+EVENING_HOUR_2 = 16  # 4 عصراً
+EVENING_HOUR_3 = 17  # 5 مساءً
+EVENING_HOUR_4 = 18  # 6 مساءً
+EVENING_HOUR_5 = 19  # 7 مساءً
 
-# فترة السماح (بالدقائق)
-GRACE_MINUTES = 40   # 50 دقيقة تسامح
+# فترة السماح
+GRACE_MINUTES = 50
