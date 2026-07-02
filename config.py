da037c12eae9400b28a38c692724047d6f6c278b
mahlotfi801-droid <mah.lotfi801@gmail.com>
2026-07-03 00:54:30 +0300

    config.py

--- config.py
+++ config.py
@@ -5,14 +5,13 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')  # أو CHAT_IDS
 
 # المواعيد بتوقيت مصر (8 مواعيد)
-MORNING_HOUR = 7    # 7 ظهراً
-AFTERNOON_HOUR = 20  # 9 ظهراً
-NIGHT_HOUR = 00      # 11 ظهراً
+MORNING_HOUR = 7    # 7 صباحا
 EVENING_HOUR_1 = 11  # 11 صباجاً
-EVENING_HOUR_2 = 23  # 1 عصراً
-EVENING_HOUR_3 = 16  # 4 مساءً
-EVENING_HOUR_4 = 18  # 7 مساءً
-EVENING_HOUR_5 = 22  # 10 مساءً
-
+EVENING_HOUR_2 = 15  # 3 عصراً
+AFTERNOON_HOUR = 17  # 5 ظهراً
+EVENING_HOUR_4 = 18  # 6 مساءً
+EVENING_HOUR_5 = 20  # 8 مساءً
+EVENING_HOUR_3 = 22  # 10 مساءً
+NIGHT_HOUR = 00      # 12 في منصف الليلاً
 # فترة السماح
-GRACE_MINUTES = 60
+GRACE_MINUTES = 55
